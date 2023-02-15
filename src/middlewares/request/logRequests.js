b93175ef966820ccb1d47ec0007e23ba84ad58cc
Cleber Henrique <cleberschiavon@outlook.com>
2023-02-15 01:46:17 -0300

    feat: creating logRequests middleware

---
+++ src/middlewares/request/logRequests.js
@@ -0,0 +1,17 @@
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  
+  request.on('finish', () => {
+    return 'Cabo'
+  })
+
+  const logLabel = `Requisição feita: [${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next(); // Next serve pra falar pro express ir pra próxima linha de código
+  console.timeEnd(logLabel);
+}
+
+module.exports = logRequests;
